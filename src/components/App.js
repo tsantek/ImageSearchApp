23a677123a72df0782a8019c436ce0ccf1f4d65a
tsantek <santek.vg@gmail.com>
2019-07-05 16:50:15 -0700

    create react app , create input and connect it with state

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SearchBar from './SearchBar';
+
+
+const App = () =>{
+    return (
+    <div className="ui container" style={{ marginTop: 20}}>
+        <SearchBar />
+    </div>
+    )
+}
+
+export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+
+class SearchBar extends Component {
+
+    state = {
+        term: ''
+    }
+
+    render() { 
+        return (
+        <div className="ui segment">
+            <form className="ui form">
+                <div className="field">
+                <label>Image Search</label>
+                <input type="text" value={this.state.term} onChange={e=> this.setState({term: e.target.value})}/>
+                </div>
+            </form>
+        </div> 
+        );
+    }
+}
+ 
+export default SearchBar;
