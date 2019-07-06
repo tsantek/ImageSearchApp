85c4d32d478a35704abf7c0127c9970bbb296a2f
tsantek <santek.vg@gmail.com>
2019-07-05 17:41:32 -0700

    finish connecting searchBar to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, {Component}from 'react'
 import SearchBar from './SearchBar';
 
 
-const App = () =>{
-    return (
-    <div className="ui container" style={{ marginTop: 20}}>
-        <SearchBar />
-    </div>
-    )
-}
+class App extends Component {
 
+    onSearchSubmit(term){
+        console.log(term)
+    }
+
+
+    render() { 
+        return (
+        <div className="ui container" style={{ marginTop: 20}}>
+            <SearchBar onSubmit={this.onSearchSubmit}/>
+        </div>
+          );
+    }
+}
+ 
 export default App;
